dc889db4f0f40025ab5c78639fd419792cff2557
Parth Awasthi <107567458+NotHumanFF@users.noreply.github.com>
2023-02-12 17:50:29 +0530

    Add files via upload

---
+++ Password Generator/index.js
@@ -0,0 +1,36 @@
+const passwordInput = document.querySelector(".password-box input"),
+  copyIcon = document.querySelector(".password-box .copy-icon"),
+  rangeInput = document.querySelector(".range-box input"),
+  sliderNumber = document.querySelector(".range-box .slider-number"),
+  generateButton = document.querySelector(".generate-button");
+
+let allCharacters =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789^!$%&|[](){}:;.,*+-#@<>~";
+
+const generatePassword = () => {
+  generateButton.innerText = "Copy Password"
+
+  let newPassword = "";
+
+  for (let i = 0; i < rangeInput.value; i++) {
+    let randomNumbers = Math.floor(Math.random() * allCharacters.length);
+    newPassword += allCharacters[randomNumbers];
+  }
+  passwordInput.value = newPassword;
+};
+
+rangeInput.addEventListener("input", () => {
+  sliderNumber.innerText = rangeInput.value;
+  generatePassword();
+});
+
+
+copyIcon.addEventListener("click", () => {
+  navigator.clipboard.writeText(passwordInput.value);
+});
+
+generatePassword();
+generateButton.addEventListener("click", () => {
+  navigator.clipboard.writeText(passwordInput.value);
+  generateButton.innerText = "Copied"
+});
